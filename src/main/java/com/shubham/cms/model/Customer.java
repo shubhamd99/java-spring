4c756c11434b8f6f2f3ae0a0b9a6fb5f04955fd6
Shubham Dhage <shubham.dhage@swiggy.in>
2021-02-13 20:29:37 +0530

    Add model for customer object

---
+++ src/main/java/com/shubham/cms/model/Customer.java
@@ -0,0 +1,41 @@
+package com.shubham.cms.model;
+
+public class Customer {
+
+    private int customerId;
+    private String customerFirstName;
+    private String customerLastName;
+    private String customerEmail;
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerFirstName() {
+        return customerFirstName;
+    }
+
+    public void setCustomerFirstName(String customerFirstName) {
+        this.customerFirstName = customerFirstName;
+    }
+
+    public String getCustomerLastName() {
+        return customerLastName;
+    }
+
+    public void setCustomerLastName(String customerLastName) {
+        this.customerLastName = customerLastName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+}
