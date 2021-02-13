00d368f19a9704a60bcc5ecb64f6ab46f27eade6
Shubham Dhage <shubham.dhage@swiggy.in>
2021-02-13 22:34:46 +0530

    Add JsonProperty annotation to rename the object properties

--- src/main/java/com/shubham/cms/model/Customer.java
+++ src/main/java/com/shubham/cms/model/Customer.java
@@ -1,10 +1,16 @@
 package com.shubham.cms.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Customer {
 
+    @JsonProperty("id")
     private int customerId;
+    @JsonProperty("firstName")
     private String customerFirstName;
+    @JsonProperty("lastName")
     private String customerLastName;
+    @JsonProperty("email")
     private String customerEmail;
 
     public int getCustomerId() {
